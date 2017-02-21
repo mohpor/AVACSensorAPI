509e3d5df537207fb769033afd57906d2e7b038d
M. Porooshani <porooshani@gmail.com>
2017-02-21 09:12:47 +0330

    Removed select queries left from APP API

--- Sources/db.swift
+++ Sources/db.swift
@@ -46,66 +46,6 @@ struct DataBase {
         static let date = 5
         static let hour = 6
 
-
-      }
-
-      static func selectBaseQuery(unixTime: Bool = true) -> String {
-
-        let dateStr = unixTime ? "UNIX_TIMESTAMP(\(Fields.date)) as \(Fields.date)" : "\(Fields.date)"
-        return "SELECT \(Fields.deviceID), \(Fields.temperature), \(Fields.humidity), \(Fields.pressure), \(Fields.uv), \(dateStr) from \(SensorDataSchema.name) "
-
-      }
-
-      static func selectAverageBaseQuery() -> String {
-
-        return "SELECT \(Fields.deviceID), AVG(\(Fields.temperature)) as \(Fields.temperature), AVG(\(Fields.humidity)) as \(Fields.humidity), AVG(\(Fields.pressure)) as \(Fields.pressure), AVG(\(Fields.uv)) as \(Fields.uv) from \(SensorDataSchema.name) "
-        
-      }
-
-
-      static func selectLastNQuery(deviceID: String, count: Int = 1) -> String {
-        var result = selectBaseQuery()
-        result += "WHERE \(Fields.deviceID) = '\(deviceID)' ORDER BY date desc Limit \(count)"
-        return result
-      }
-
-
-      static func selectRangeQuery(deviceID: String, startDate: Double, endDate: Double?, unixTime: Bool = true) -> String {
-
-        var result = selectBaseQuery(unixTime: unixTime)
-
-        result += "WHERE \(Fields.deviceID) = '\(deviceID)' "
-        result += "and \(Fields.date) >= \(startDate) "
-        if let ed = endDate {
-          result += "and \(Fields.date) <= \(ed) "
-        }
-        result += "ORDER BY date desc"
-
-        return result
-
-      }
-
-      static func selectHourlyAverageQuery(deviceID: String, startDate: Double, endDate: Double?) -> String {
-        var result = "Select t.\(Fields.deviceID), AVG(t.\(Fields.temperature)) as \(Fields.temperature), AVG(t.\(Fields.humidity)) as \(Fields.humidity), AVG(t.\(Fields.pressure)) as \(Fields.pressure), AVG(t.\(Fields.uv)) as \(Fields.uv), DATE_FORMAT(t.\(Fields.date), '%Y-%m-%d') as \(Fields.date_part), HOUR(t.\(Fields.date)) as \(Fields.date_hour) from"
-
-        result += "(\(selectRangeQuery(deviceID: deviceID, startDate: startDate, endDate: endDate, unixTime: false))) "
-        result += "t GROUP BY \(Fields.date_part),\(Fields.date_hour)"
-        return result
-      }
-
-      static func selectAverageRangeQuery(deviceID: String, startDate: Double, endDate: Double?) -> String {
-
-        var result = selectAverageBaseQuery()
-
-        result += "WHERE \(Fields.deviceID) = '\(deviceID)' "
-        result += "and UNIX_TIMESTAMP(\(Fields.date)) >= \(startDate) "
-        if let ed = endDate {
-          result += "and UNIX_TIMESTAMP(\(Fields.date)) <= \(ed) "
-        }
-        result += "ORDER BY date desc"
-
-        return result
-        
       }
 
       static func insertQuery(sensorData: SensorData) -> String {
